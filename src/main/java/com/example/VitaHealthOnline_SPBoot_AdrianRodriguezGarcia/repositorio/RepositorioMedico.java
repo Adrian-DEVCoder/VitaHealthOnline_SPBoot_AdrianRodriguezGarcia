33b569d5ad37a17232d3b41ad16baf9801db348b
Dctr-LurS <adrianrg2004@gmail.com>
2024-03-05 10:49:28 +0100

    Implementacion del repositorio y servicio de Medico

--- src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/repositorio/RepositorioMedico.java
+++ src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/repositorio/RepositorioMedico.java
@@ -1,4 +1,10 @@
 package com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.repositorio;
 
-public interface RepositorioMedico {
+import com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.entidades.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioMedico extends JpaRepository<Medico, Integer> {
+    // No es necesario definir métodos personalizados para operaciones básicas de CRUD
 }
--- src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/servicios/ServicioMedico.java
+++ src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/servicios/ServicioMedico.java
@@ -1,4 +1,37 @@
 package com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.servicios;
 
+import com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.entidades.Medico;
+import com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.repositorio.RepositorioMedico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ServicioMedico {
+
+    private final RepositorioMedico repositorioMedico;
+
+    @Autowired
+    public ServicioMedico(RepositorioMedico repositorioMedico) {
+        this.repositorioMedico = repositorioMedico;
+    }
+
+    public boolean insertarActualizarMedico(Medico medico) {
+        repositorioMedico.save(medico);
+        return true;
+    }
+
+    public List<Medico> getAllMedicos() {
+        return repositorioMedico.findAll();
+    }
+
+    public Medico getMedico(int id) {
+        return repositorioMedico.findById(id).orElse(null);
+    }
+
+    public boolean deleteMedico(int id) {
+        repositorioMedico.deleteById(id);
+        return true;
+    }
 }
