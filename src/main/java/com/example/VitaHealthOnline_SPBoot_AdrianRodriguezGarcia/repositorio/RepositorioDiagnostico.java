4f0206115b2302347c24aaf00270d82934846615
Dctr-LurS <adrianrg2004@gmail.com>
2024-03-05 10:43:22 +0100

    Implementacion del repositorio y servicio de Diagnostico

--- src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/repositorio/RepositorioDiagnostico.java
+++ src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/repositorio/RepositorioDiagnostico.java
@@ -1,4 +1,12 @@
 package com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.repositorio;
 
-public interface RepositorioDiagnostico {
+import com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.entidades.Diagnostico;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RepositorioDiagnostico extends JpaRepository<Diagnostico, Integer> {
+    List<Diagnostico> findByHistorialIdHistorial(int id);
 }
--- src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/servicios/ServicioDiagnostico.java
+++ src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/servicios/ServicioDiagnostico.java
@@ -1,4 +1,41 @@
 package com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.servicios;
 
+import com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.entidades.Diagnostico;
+import com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.repositorio.RepositorioDiagnostico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ServicioDiagnostico {
+
+    private final RepositorioDiagnostico repositorioDiagnostico;
+
+    @Autowired
+    public ServicioDiagnostico(RepositorioDiagnostico repositorioDiagnostico) {
+        this.repositorioDiagnostico = repositorioDiagnostico;
+    }
+
+    public boolean insertarActualizarDiagnostico(Diagnostico diagnostico) {
+        repositorioDiagnostico.save(diagnostico);
+        return true;
+    }
+
+    public List<Diagnostico> getAllDiagnosticos() {
+        return repositorioDiagnostico.findAll();
+    }
+
+    public Diagnostico getDiagnostico(int id) {
+        return repositorioDiagnostico.findById(id).orElse(null);
+    }
+
+    public boolean deleteDiagnostico(int id) {
+        repositorioDiagnostico.deleteById(id);
+        return true;
+    }
+
+    public List<Diagnostico> getDiagnosticosByHistorial(int id) {
+        return repositorioDiagnostico.findByHistorialIdHistorial(id);
+    }
 }
