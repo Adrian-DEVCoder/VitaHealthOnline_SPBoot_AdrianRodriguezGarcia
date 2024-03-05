1dc535104133cd144a9a6a210e79097b8cb71e42
Dctr-LurS <adrianrg2004@gmail.com>
2024-03-05 10:04:34 +0100

    Initial commit

---
+++ src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/VitaHealthOnlineSpBootAdrianRodriguezGarciaApplication.java
@@ -0,0 +1,13 @@
+package com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitaHealthOnlineSpBootAdrianRodriguezGarciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitaHealthOnlineSpBootAdrianRodriguezGarciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/VitaHealthOnlineSpBootAdrianRodriguezGarciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitaHealthOnlineSpBootAdrianRodriguezGarciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
