47f4cd36f5402204aa77094776a55a55af06af67
Dctr-LurS <adrianrg2004@gmail.com>
2024-03-05 10:37:33 +0100

    Implementacion del repositorio y servicio de consultas

--- src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/repositorio/RepositorioConsulta.java
+++ src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/repositorio/RepositorioConsulta.java
@@ -1,4 +1,11 @@
 package com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.repositorio;
 
-public interface RepositorioConsulta {
+import com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.entidades.Consulta;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepositorioConsulta extends JpaRepository<Consulta, Integer> {
+    List<Consulta> findByPacienteIdPaciente(int id);
 }
--- src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/servicios/ServicioConsulta.java
+++ src/main/java/com/example/VitaHealthOnline_SPBoot_AdrianRodriguezGarcia/servicios/ServicioConsulta.java
@@ -1,4 +1,39 @@
 package com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.servicios;
 
+import com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.entidades.Consulta;
+import com.example.VitaHealthOnline_SPBoot_AdrianRodriguezGarcia.repositorio.RepositorioConsulta;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServicioConsulta {
+    private final RepositorioConsulta repositorioConsulta;
+
+    @Autowired
+    public ServicioConsulta(RepositorioConsulta repositorioConsulta) {
+        this.repositorioConsulta = repositorioConsulta;
+    }
+
+    public boolean insertarActualizarConsulta(Consulta consulta) {
+        repositorioConsulta.save(consulta);
+        return true;
+    }
+
+    public List<Consulta> getAllConsultas() {
+        return repositorioConsulta.findAll();
+    }
+
+    public Consulta getConsulta(int id) {
+        return repositorioConsulta.findById(id).orElse(null);
+    }
+
+    public boolean deleteConsulta(int id) {
+        repositorioConsulta.deleteById(id);
+        return true;
+    }
+
+    public List<Consulta> obtenerConsultasPorPaciente(int id) {
+        return repositorioConsulta.findByPacienteIdPaciente(id);
+    }
 }
